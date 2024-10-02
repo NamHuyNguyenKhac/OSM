c458ccfc72f0b344e882af39800b89bb713f1d4f
Nam Huyy <101095378+NamHuyNguyenKhac@users.noreply.github.com>
2024-10-02 14:17:41 +0700

    Add

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template
+import psycopg2
 
 app = Flask(__name__)
+
+# Connect to PostgreSQL
+conn = psycopg2.connect(dbname="osm_db", user="postgres", password="665667", host="localhost")
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/get_parks')
+def get_parks():
+    cur = conn.cursor()
+    cur.execute("SELECT ST_AsGeoJSON(way) FROM planet_osm_polygon WHERE leisure='park';")
+    parks = cur.fetchall()
+    cur.close()
+    return jsonify(parks)
 
 if __name__ == '__main__':
     app.run(debug=True)
